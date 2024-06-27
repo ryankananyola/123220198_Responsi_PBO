80673f29f514f0293c150045d3ffc9f50155293e
ryankananyola <mariadestan2027@gmail.com>
2024-06-27 22:35:38 +0700

    Program Done (main)

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,6 @@
 package Main;
 
-import View.Dosen.ViewDataDosen;
 import View.LoginPage;
-import View.Mahasiswa.EditDataMahasiswa;
-import View.Mahasiswa.InputDataMahasiswa;
-import View.Mahasiswa.ViewDataMahasiswa;
 
 public class Main {
 
